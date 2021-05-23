d20d1fe8fc9677c7b79cba04a95d509325501a84
Putri Ayu Nengsih <putriayunengsih>
2021-05-23 20:37:51 +0800

    perbaikan MainActivity

--- app/src/main/java/com/example/tugas1_pam/MainActivity.kt
+++ app/src/main/java/com/example/tugas1_pam/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
+        inflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
