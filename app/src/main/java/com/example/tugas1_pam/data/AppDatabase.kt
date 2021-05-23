30c2a2f817e94be497ab80946f8d7703a8e8b387
Putri Ayu Nengsih <putriayunengsih>
2021-05-23 18:43:04 +0800

    NoteDatabase

---
+++ app/src/main/java/com/example/tugas1_pam/data/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.tugas1_pam.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], exportSchema = false, version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+
+        private const val AppDatabase = "NOTE_DB"
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (instance == null) {
+                synchronized(AppDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        "AppDatabase"
+                    ).build()
+                }
+            }
+            return instance
+        }
